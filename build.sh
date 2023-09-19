6304db5171459cf26326547852a42ce99940ce85
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-09-19 18:46:06 +0200

    fix typst download

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/typst/typst/releases/blob/uwu -o typst.tar.gz
-tar -xzf typst.tar.gz
+curl -L https://github.com/typst/typst/releases/download/$TYP_VERSION/typst-x86_64-unknown-linux-musl.tar.xz -o typst.tar.xz
+tar -xf typst.tar.xz
 
-./typst compile "$1" "$2"
+./typst-x86_64-unknown-linux-musl/typst compile "$1" "$2"
